25bcda0ab7910cb4f5d153a8730e422c7ec3cb83
Tam Vo Minh <tam.vominh@gameloft.com>
2025-01-07 14:20:04 +0700

    change to work with Unity 6

--- UPMPackage/Scripts/Editor/RTLTextMeshProEditor.cs
+++ UPMPackage/Scripts/Editor/RTLTextMeshProEditor.cs
@@ -2,11 +2,7 @@
 using UnityEditor;
 using UnityEngine;
 
-#if TMP_VERSION_2_1_0_OR_NEWER
 using TMP_UiEditorPanel = TMPro.EditorUtilities.TMP_EditorPanelUI;
-#else
-using TMP_UiEditorPanel = TMPro.EditorUtilities.TMP_UiEditorPanel;
-#endif
 
 namespace RTLTMPro
 {
